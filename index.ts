d5074d850d6270508578fe4d3beb8006d180650c
Ushio Cheng <ushio.cheng@gmail.com>
2021-05-09 14:26:53 -0700

    completed file hash function

--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
-console.log("Hello World!")
+import fs from "fs/promises";
+import crypto from "crypto";
+
+const getFileHash = async (filepath: string) => {
+    const fileBuffer = await fs.readFile(filepath);
+    const hashResult = crypto.createHash("sha256");
+    hashResult.update(fileBuffer);
+    return hashResult.digest("hex") as string;
+};
+
+(async () => {
+    console.log(`f1: ${await getFileHash("./f1.webp")}`);
+    console.log(`f2: ${await getFileHash("./f2.webp")}`);
+})();
